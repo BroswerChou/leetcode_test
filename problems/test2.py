b4df714df13b138d278fc584a9a9cbc3cb029631
BroswerChou <BroswerChou@guigu.com>
2024-01-12 17:18:34 +0800

    Initial commit

---
+++ problems/test2.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+def addTwoNumbers(self, l1, l2) :
+    cur = dummy = ListNode()  # 哨兵节点
+    carry = 0  # 进位
+    while l1 or l2 or carry:  # 有一个不是空节点，或者还有进位，就继续迭代
+        carry += (l1.val if l1 else 0) + (l2.val if l2 else 0)  # 节点值和进位加在一起
+        cur.next = ListNode(carry % 10)  # 每个节点保存一个数位
+        carry //= 10  # 新的进位
+        cur = cur.next  # 下一个节点
+        if l1: l1 = l1.next  # 下一个节点
+        if l2: l2 = l2.next  # 下一个节点
+    return dummy.next  # 哨兵节点的下一个节点就是头节点
